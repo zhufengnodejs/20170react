7ea49df6c39550585a23ff70fb02eb63927cbef1
zhufengnodejs <zhufengnodejs@126.com>
2017-06-29 17:20:56 +0800

    添加消息

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,10 +1,17 @@
-import React,{Component} from 'react';
-export default class Slider extends Component{
-    render(){
-        return (
-            <div>
-                slider
-            </div>
-        )
-    }
+import React, {Component} from 'react';
+import './slider.css';
+export default class Slider extends Component {
+ render() {
+  return (
+   <div className="slider-wrapper">
+    <ul className="sliders">
+     {
+      this.props.items.map((item,index)=>(
+       <li className="slider"><img src={item.src}/></li>
+      ))
+     }
+    </ul>
+   </div>
+  )
+ }
 }
