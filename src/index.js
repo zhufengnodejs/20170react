6b2eba10d06a87cdede2bfe054fce6e29a119b3f
zhufengnodejs <zhufengnodejs@126.com>
2017-07-04 10:06:52 +0800

    添加消息

--- src/index.js
+++ src/index.js
@@ -1,25 +1,41 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 class PercentageShower extends Component {
-  render(){
+  render() {
     return (
       <div>
-
+        {this.props.percent}
       </div>
     )
   }
 }
 class Input extends Component {
+  handleChange = (event) => {
+    this.props.change((event.target.value * 100).toFixed(2) + '%');
+  }
+
   render() {
-    return <input type="number"/>
+    return <input type="number" onChange={this.handleChange}/>
   }
 }
 class PercentageApp extends Component {
+  constructor(props) {
+    super();
+    //定义一个默认状态
+    this.state = {percent: '30%'};
+  }
+
+  change = (percent) => {
+    this.setState({percent});
+  }
+
   render() {
     return (
       <div>
-        <Input/>
-        <PercentageShower/>
+        <Input change={this.change}/>
+        <PercentageShower percent={this.state.percent}/>
       </div>
     )
   }
 }
+ReactDOM.render(<PercentageApp/>, root);
