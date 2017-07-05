72c1b5032e7a9e2d032f8b394d5900789ad37e9d
zhufengnodejs <zhufengnodejs@126.com>
2017-07-05 15:52:20 +0800

    实现todo

--- src/connect.js
+++ src/connect.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import store from './store';
+//connect是为了连接的仓库里的状态村和组件的属性对象
+//为了减少重复代码
+//输入 就是把redux状态数中的属性映射或者说输入到组件中显示
+//输出 把当前组件中的操作输出出去，从而修改状态树
+// mapStateToProps 把redux的状态树映射为自己的状态对象，然后作为属性传递给_component
 let connect = (mapStateToProps,mapDispatchToProps) => (_component) => {
   class Proxy extends React.Component {
     constructor() {
       super();
+      //store.getState() {counter:{number:0},todos:{list:[]}}
       //把状态树映射为当前Proxy组件的state，也就是_component的属性
       this.state = mapStateToProps(store.getState());
+      //this.state = {number:0}; state.counter
     }
 
     componentWillMount() {
