0a446f75f228bba504401d7bbf99137107182d33
zhufengnodejs <zhufengnodejs@126.com>
2017-07-05 12:27:44 +0800

    实现todo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import React from 'react';
-import ReactDOM,{render} from 'react-dom';
-import App from './components/App';
+import ReactDOM from 'react-dom';
+/**
+ * 生成一个高阶组件的函数 其实就是外层组件
+ * 可以接收一个普通的组件
+ **/
+let highOrder = (_component) => {
+  class Proxy extends React.Component{
 
-render(<App/>,document.querySelector('#root'));
+  }
+  return Proxy;
+}
+
+class Counter extends React.Component{
+  render(){
+    return (
+      <div>
+        <p>{this.props.number}</p>
+        <button>+</button>
+      </div>
+    )
+  }
+}
+let newCounter = highOrder(Counter);
+ReactDOM.render(newCounter,document.querySelector('#root'));
