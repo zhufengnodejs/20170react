aecf68e535f9a06c76082a844f5800328a2f482b
zhufengnodejs <zhufengnodejs@126.com>
2017-07-11 16:13:57 +0800

    做好结构

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,32 @@
 import React, {Component} from 'react';
 import './App.css'
-export default class App extends Component {
+import actions from '../store/actions';
+import {connect} from 'react-redux';
+class App extends Component {
   render() {
     return (
       <div className="container">
-        <div className="logo">
-          <img src="http://t.cn/RU9vHxQ" />
+        <div className="search-area">
+          <div className="logo">
+            <img src="http://t.cn/RU9vHxQ" />
+          </div>
+          <div className="search">
+            <input type="text" onChange={this.handleChange}/>
+            <span>百度一下</span>
+          </div>
         </div>
-        <div className="search">
-          <input type="text"/>
-          <span>百度一下</span>
-        </div>
+        <ul className="words">
+          {
+            this.props.words.map((word,index)=>(<li className="word" key={index}>{word}</li>))
+          }
+          <li className="word">联想词</li>
+        </ul>
       </div>
     )
   }
 }
+//把store里的状态映射为属性对象
+let mapStateToProps = state=>state;
+let mapDispatchToProps = actions;
+
+export default connect(mapStateToProps,mapDispatchToProps)(App)
