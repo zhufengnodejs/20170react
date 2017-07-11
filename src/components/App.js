61ec784defcf334d6be1607dd5f5dcce102fee57
zhufengnodejs <zhufengnodejs@126.com>
2017-07-11 15:53:46 +0800

    做好结构

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React, {Component} from 'react';
+import './App.css'
 export default class App extends Component {
   render() {
     return (
-      <div>
-        App
+      <div className="container">
+        <div className="logo">
+          <img src="http://t.cn/RU9vHxQ" />
+        </div>
+        <div className="search">
+          <input type="text"/>
+          <span>百度一下</span>
+        </div>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 import App from './components/App';
-
-let store = require('./store');
+import store from './store';
 render(<Provider store={store}>
   <App/>
 </Provider>,document.querySelector('#root'));
