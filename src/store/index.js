69e815f71599d26d70b5a52094b5ba49de2bc410
zhufengnodejs <zhufengnodejs@126.com>
2017-07-11 17:13:13 +0800

    做好结构

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 //从redux中引入createStore属性，它是一个函数
 import {createStore,applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import reducer from './reducer';
 //创建一个store
-let store = applyMiddleware(thunk)(createStore)(reducer);
+let store = applyMiddleware(thunk,logger)(createStore)(reducer);
 //let store = createStore(reducer);
 //默认导出store
 export default store;
