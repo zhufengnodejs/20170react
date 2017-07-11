4055bf2d059d887247930095561300e01b4cd7aa
zhufengnodejs <zhufengnodejs@126.com>
2017-07-11 17:45:22 +0800

    做好结构

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,29 @@
-import React,{Component} from 'react';
-export default class App extends Component{
-    render(){
-        return (
-            <div>
-              App
-            </div>
-        )
-    }
+import React, {Component} from 'react';
+//BrowserRouter HashRouter 浏览器路由容器
+import {
+  HashRouter as Router,//路由容器 app
+  Route,//是单条路由 app.get()
+  Link //菜单导航，超链接
+} from 'react-router-dom';
+function Home(){
+  return <div>Home</div>
+}
+function User(){
+  return <div>User</div>
+}
+function Profile(){
+  return <div>Profile</div>
+}
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route path="/" component={Home}/>
+          <Route path="/user" component={User}/>
+          <Route path="/profile" component={Profile}/>
+        </div>
+      </Router>
+    )
+  }
 }
